a09116c0465727671ccc5d9fe3d4277b4560cc0d
biff990 <133827801+biff990@users.noreply.github.com>
2023-05-17 14:22:29 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,16 @@ export default function RootLayout({
         <meta name="version" content={buildConfig.commitId} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        <script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?84c2b1c3e2c08559e4fe3ee520d40b50";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
       </head>
       <body>{children}</body>
     </html>
